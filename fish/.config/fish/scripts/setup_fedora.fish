14cce5595e9eed363d31f338d9d8fb5260595af3
NamThieen <executiveassault2@gmail.com>
2026-01-11 10:46:54 -0500

    Adding Plasma

--- fish/.config/fish/scripts/setup_fedora.fish
+++ fish/.config/fish/scripts/setup_fedora.fish
@@ -9,18 +9,6 @@ function check_status
     end
 end
 
-# ---  Remove GNOME Software autostart ---
-echo "---  Removing GNOME Software autostart entry ---"
-sudre gnome-software
-# ---  Flatpak Configuration ---
-echo "---  Configuring Flatpak remotes ---"
-echo "Removing default Fedora Flatpak remote (if it exists)..."
-sudo flatpak remote-delete fedora 2 >/dev/null || true
-echo "Adding Flathub remote..."./
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-check_status
-echo "Flathub remote added."
-
 # ---  Add Terra repository ---
 echo "---  Adding Terra repository ---"
 echo "Installing Terra release package..."
@@ -31,7 +19,6 @@ echo "Terra repository added."
 # ---  Update Flatpak appstream ---
 echo "---  Updating Flatpak appstream data ---"
 sudo flatpak update --appstream
-sudo flatpak install warehouse bazaarq
 check_status
 echo "Flatpak appstream updated."
 
@@ -40,18 +27,6 @@ echo "---  Disabling NetworkManager-wait-online.service ---"
 sudo systemctl disable NetworkManager-wait-online.service
 check_status
 echo "NetworkManager-wait-online.service disabled."
-
-# ---  Install Valent and Valent GNOME Shell Extension ---
-echo "---  Installing Valent ---"
-echo "Installing Valent via Flatpak..."
-sudo flatpak install -y https://valent.andyholmes.ca/valent.flatpakref
-check_status
-echo "Valent has been installed."
-
-# --- Restore extensions ---
-echo "---  Restoring extensions"
-sudo sudo ~/.conf/extensions/backup.sh restore ~/.conf/extensions/10-1backup/
-check_status
 
 # --- Install Vivaldi ---
 echo "--- Installing Vivaldi"
