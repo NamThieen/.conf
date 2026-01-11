27411b33ba6ed47dccd366e85d69f1da180017ff
NamThieen <executiveassault2@gmail.com>
2026-01-11 07:33:28 +0700

    Something went wrong?

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,15 @@ local config ={}
 config.font = wezterm.font 'Adwaita Mono'
 
 config.keys = {
+  {
+    key = 'q',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+config.keys = {
   { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
 }
 config.launch_menu = {
@@ -10,23 +19,8 @@ config.launch_menu = {
     args = { 'top' },
   },
   {
-    -- Optional label to show in the launcher. If omitted, a label
-    -- is derived from the `args`
     label = 'Bash',
-    -- The argument array to spawn.  If omitted the default program
-    -- will be used as described in the documentation above
     args = { 'bash', '-l' },
-
-    -- You can specify an alternative current working directory;
-    -- if you don't specify one then a default based on the OSC 7
-    -- escape sequence will be used (see the Shell Integration
-    -- docs), falling back to the home directory.
-    -- cwd = "/some/path"
-
-    -- You can override environment variables just for this command
-    -- by setting this here.  It has the same semantics as the main
-    -- set_environment_variables configuration option described above
-    -- set_environment_variables = { FOO = "bar" },
   },
 }
 config.hide_tab_bar_if_only_one_tab = true
